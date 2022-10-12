a860caa317475c2732b9b0b6ae5c46bd4db45446
Marcx <haitianhero6@gmail.com>
2022-10-11 22:23:16 -0400

    mocks and tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,10 @@ require("hardhat-contract-sizer");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -23,8 +22,8 @@ module.exports = {
     goerli: {
       chainId: 5,
       blockConfirmations: 6,
-      url:
-      accounts:
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
   solidity: "0.8.8",
